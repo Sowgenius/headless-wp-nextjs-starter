c2215a041963ab86c5f1d0020ca4065bef500fc0
Sowgenius <korkasow1@gmail.com>
2022-08-26 20:15:46 +0000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: false,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll:1000,
+      aggregateTimeout : 300
+    }
+    return config
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
