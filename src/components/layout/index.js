bfdd85b1500b880257f4051e222d457308ac452f
Sowgenius <korkasow1@gmail.com>
2023-07-04 01:31:13 +0000

    fixing footer v4

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,6 @@
 //import { AppProvider } from '../context';
 import Header from "./Header";
-
+import Footer from "./Footer";
 
 const Layout = ({children}) => {
 	//const { header, footer } = headerFooter || {};
@@ -9,6 +9,7 @@ const Layout = ({children}) => {
       <div>
         <Header />
         <main className="container mx-auto py-4 min-h-50vh">{children}</main>
+        <Footer />
       </div>
     </>
   );
